56c9ccacefa937c87438d50ef2a7f21bad580bb6
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-05-13 18:06:14 +0200

    final

--- Exercice_3.py
+++ Exercice_3.py
@@ -2,33 +2,32 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
 class Window(QWidget):
-
     def __init__(self):
         QWidget.__init__(self)
 
         self.setFixedSize(400,300)
         self.setWindowTitle("Charging...")
 
-        self.icon = QIcon("C:\Documents\PyCharm Projects\drapeau-fr.png")
-        self.layout = QBoxLayout()
-        self.label = QLabel("Hello World")
-        self.progressbar = QProgressBar()
-        self.lineEdit = QLineEdit()
-        self.button = QPushButton("Click me !")
+        icon = QIcon("C:\Documents\PyCharm Projects\drapeau-fr.png")
+        layout = QGridLayout()
+        label = QLabel("Hello World")
+        progressBar = QProgressBar()
+        lineEdit = QLineEdit()
+        button = QPushButton("Click me !")
 
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.progressbar)
-        self.layout.addWidget(self.lineEdit)
-        self.layout.addWidget(self.button)
+        layout.addWidget(label)
+        layout.addWidget(progressBar)
+        layout.addWidget(lineEdit)
+        layout.addWidget(button)
 
-        self.setWindowIcon(self.myicon)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.progressbar.setValue(50)
-        self.button.setToolTip("YES CLICK!")
-        self.setLayout(self.layout)
+        progressBar.setValue(50)
+        button.setToolTip("YES HERE !")
+        self.setWindowIcon(icon)
+        label.setAlignment(Qt.AlignCenter)
+        self.setLayout(layout)
 
-if __name__ == "_main_":
-    app = QApplication([])
-    window = Window()
-    window.show()
-    app.exec_()
+if __name__ == "__main__":
+   app = QApplication([])
+   window = Window()
+   window.show()
+   app.exec_()
